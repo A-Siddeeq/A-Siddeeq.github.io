c41f67582bff738237abb3a56626500b0c096435
A-siddeeq <abubakaradamu1163@gmail.com>
2024-04-23 10:43:34 +0100

    Fixed bug in router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './pages/About'
 import Work from './pages/Work'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
-import { HashRouter as Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
  
